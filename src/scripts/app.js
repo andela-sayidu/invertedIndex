d92463a3399a9def026a3f5df918e214cc11cc71
andela-sayidu <shalom.ayidu@andela.com>
2016-11-21 23:53:16 +0100

    Generate Tables based on uploaded files

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,31 +4,35 @@ app.controller('indexController', ($scope) => {
     const index = new invertedIndex();
 
     $scope.titles = [];
-    $scope.data = '';
-    $scope.filesLength = [];
-    let fileNo = 0;
+    $scope.selectedFiles ='';
 
-    $scope.createIndex = (fileName, fileContent) => {
+
+
+    $scope.uploadFile = (fileName, fileContent) => {
+        $scope.data ={};
+        $scope.docCount = [];
+
         if (fileName.toLowerCase().match(/\.json$/)) {
             const reader = new FileReader();
             reader.onload = (e) => {
                 let content = JSON.parse(e.target.result);
-                $scope.file =  index.createIndex(fileName, content);
-                $scope.data =index.indexMap[fileName];
-                console.log($scope.data);
+                index.createIndex(fileName, content);
+                $scope.data = index.indexMap[fileName];
 
+                for (let fileNo = 0; fileNo < content.length; fileNo++) {
+                    $scope.docCount.push(fileNo);
+                }
+                $scope.$apply($scope.docCount);
             }
-    
-            
             reader.readAsText(fileContent);
+
 
             $scope.$apply(() => {
                 $scope.titles.push(fileName);
-                $scope.filesLength.push(fileNo);
-                $scope.data;
+                 $scope.selectedFiles=$scope.selectedFile;
+                 console.log($scope.selectedFiles);
             });
 
-            fileNo += 1;
         } else {
             $(document).ready(function () {
                 $('#modal1').modal('open');
@@ -36,12 +40,12 @@ app.controller('indexController', ($scope) => {
         }
     }
 
-     $scope.search = () => {
-            alert("dfffd");
-     }
-    
-});
+    $scope.search = () => {
+        let searchValue = $scope.terms;
+        $scope.searchResult = index.searchIndex($scope.fileNme, searchValue);
+    }
 
+});
 
 
 
@@ -52,7 +56,6 @@ document.addEventListener('DOMContentLoaded', () => {
         .addEventListener('change', function (e) {
             let fileContent = e.target.files[0];
             let fileName = e.target.files[0].name;
-            angular.element(document.getElementById('uploadJSON')).scope().createIndex(fileName, fileContent);
-
+            angular.element(document.getElementById('uploadJSON')).scope().uploadFile(fileName, fileContent);
         });
 });
--- src/scripts/inverted-index.js
+++ src/scripts/inverted-index.js
@@ -3,87 +3,86 @@
 */
 class invertedIndex {
 
-	constructor() {
-		this.indexMap = {};
-	}
+  constructor() {
+    this.indexMap = {};
+  }
 
 
-	sanitize(indexes) {
-		let lowerindex = [];
-		indexes.map((word) => {
-			lowerindex.push(word.toLowerCase().replace(/[!''@#$%^&*,'.]/g, ""));
-		});
-		return lowerindex;
-	}
+  sanitize(indexes) {
+    let lowerindex = [];
+    indexes.map((word) => {
+      lowerindex.push(word.toLowerCase().replace(/[!''@#$%^&*,'.]/g, ""));
+    });
+    return lowerindex;
+  }
 
 	/*
 	* Create File Index
 	*/
-	createIndex(fileName, fileContent) {
-		let completeIndex = [];
-		for (let value of fileContent) {
-			let title = value.title;
-			let splitTitle = title.split(' ');
-			let textTitle = this.sanitize(splitTitle);
+  createIndex(fileName, fileContent) {
+    let completeIndex = [];
+    for (let value of fileContent) {
+      let title = value.title;
+      let splitTitle = title.split(' ');
+      let textTitle = this.sanitize(splitTitle);
 
-			let text = value.text;
-			let splitText = text.split(' ');
-			let content = this.sanitize(splitText);
+      let text = value.text;
+      let splitText = text.split(' ');
+      let content = this.sanitize(splitText);
 
-			let mergeWords = content.concat(textTitle);
-			completeIndex.push(mergeWords);
-		}
-		this.storeIndex(fileName, completeIndex);
-		return [fileName, completeIndex];
-	}
+      let mergeWords = content.concat(textTitle);
+      completeIndex.push(mergeWords);
+    }
+    this.storeIndex(fileName, completeIndex);
+    return [fileName, completeIndex];
+  }
 
-	storeIndex(textTitle, completeIndex) {
-		const words = {}
-		for (let pos in completeIndex) {
-			completeIndex[pos].forEach((word) => {
-				if (words[word]) {
-					if (words[word].indexOf(pos) === -1) {
-						words[word].push(pos);
-					}
-				} else {
-					words[word] = [pos];
-				}
-			});
-		}
-		return this.indexMap[textTitle] = words;
-	}
+  storeIndex(textTitle, completeIndex) {
+    const words = {}
+
+    for (let pos in completeIndex) {
+      completeIndex[pos].forEach((word) => {
+        if (words[word]) {
+          if (words[word].indexOf(pos) === -1) {
+            words[word].push(pos);
+          }
+        } else {
+          words[word] = [pos];
+        }
+      });
+    }
+    return this.indexMap[textTitle] = words;
+  }
 
 	/*
 	* Get File Index
 	*/
-	getIndex(title) {
-	  if(!this.indexMap[title]){
-	      return "wrong";
-	   }else{
-		  return this.indexMap[title];
-	   }
-	}
+  getIndex(title) {
+    return this.indexMap[title];
+  }
 
 
-	/*
-	* Search for particular words 
-	*/
-	searchIndex(terms) {
-		const searchResult = {};
-		let a = terms.split(' ');
-		var result = this.indexMap;
-		let keys = Object.keys(result);
+	/**
+	 * Search for Terms
+	 */
+  searchIndex(terms) {
+    const searchResult = {};
+    let a = terms.split(' ');
+    var result = this.indexMap;
+    let keys = Object.keys(result);
 
-		for (let key in keys) {
-			if (this.getIndex(keys[key])) {
-				a.forEach((term) => {
-					if (result[keys[key]][term]) {
-						searchResult[term] = result[keys[key]][term];
-					}
-				});
-			}
-		}
-	}
+    for (let key in keys) {
+      a.forEach((term) => {
+        if (result[keys[key]][term]) {
+          searchResult[term] = result[keys[key]][term];
+        }
+      });
+    }
+    return searchResult;
+  }
+
+
+
 
 }
 
