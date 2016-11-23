8ec951cb0b0d8970ee17230b3e9c6c1e7a507acf
andela-sayidu <shalom.ayidu@andela.com>
2016-11-24 00:27:16 +0100

    feature(search):Applyfeedback from peer review

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: './src',
 
 
     // frameworks to use
@@ -13,8 +13,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './src/scripts/inverted-index.js',
-      './jasmine/spec/app-test.js'
+      './scripts/inverted-index.js',
+      '../jasmine/spec/app-test.js'
     ],
 
 
