8e45967ff1cbfbc3f889ad4a86f40e084a76794e
andela-sayidu <shalom.ayidu@andela.com>
2016-11-23 18:49:45 +0100

    feature(search):Address coverage issues

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -53,6 +53,6 @@ gulp.task('browserify', () =>
 );
 
 gulp.task('testApp', ['browserify'], () => {
-    gulprun('node_modules/karma/bin/karma start karma.conf.js --single-run').exec();
+    gulprun('karma start karma.conf.js --single-run').exec();
 });
 
